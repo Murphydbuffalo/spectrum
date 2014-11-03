73847872805c81ee9763f8e588cf7904d36887af
Dan Murphy <murphydbuffalo@gmail.com>
2014-11-03 13:52:55 -0500

    Set up server file (spectrum.rb) with required libraries and routes.

--- spectrum.rb
+++ spectrum.rb
@@ -5,5 +5,17 @@ require 'pony'
 Dotenv.load
 
 get '/' do
-  
+  redirect '/home'
+end
+
+get '/home' do
+  erb :home
+end
+
+get '/residential' do
+  erb :residential
+end
+
+get '/commercial' do
+  erb :commercial
 end
